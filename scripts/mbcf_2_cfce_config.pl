e7a9780d6a7093e58decd5138ff8012899c4f371
Zach_Herbert <zth1@mbcf-s1.(none)>
2015-12-16 15:40:36 -0500

    added script to convert mbcf config.yaml to supported config.yaml

---
+++ scripts/mbcf_2_cfce_config.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+#
+use strict;
+use warnings;
+use Getopt::Long;
+use Cwd;
+use YAML::Tiny;
+
+my $options = parse_options();
+my $yaml = YAML::Tiny -> read( $$options{ 'config' } );
+my $meta_info = undef;
+($yaml, $meta_info ) = get_meta_info( $yaml, $$options{'metasheet'} );
+print_meta_info( "metasheet.csv", $meta_info );
+$yaml -> write( "snakemake/config.yaml" );
+exit $?;
+
+sub print_meta_info {
+	my( $meta_file, $meta_info ) = @_;
+	open( OFH, ">$meta_file" ) or die "Error in writing to the file, $meta_file, $!\n";
+	print OFH join( "\n", @$meta_info );
+	close OFH or die "Error in closingt he file, $meta_file, $!\n";
+}
+
+sub parse_options {
+	my $options = {};
+	GetOptions( $options, 'config|c=s', 'metasheet|m=s', 'help|h' );
+	unless( $$options{ 'config' } or $$options{ 'metasheet' } ) {
+		print STDERR "Usage: $0 <--config|-c> <--metasheet|-m>\n";
+		exit 1;
+	}
+	return $options;
+}
+
+sub get_meta_info {
+	my( $yaml, $meta_file ) = @_;
+	my $meta_info = [];
+	open( FH, "<$meta_file" ) or die "Error in opening the file, $meta_file, $!\n";
+	my $header = <FH>;
+	chomp $header;
+	my @head = split( ",", $header );
+	push @$meta_info, join(",", @head[1..scalar @head - 1] );
+	my $sample_info = {};
+	while( my $line = <FH> ) {
+		chomp $line;
+		if( $line =~ /^\s+$/ ) {
+			next;
+		} else {
+			my( $left_mate, $sample, @rest ) = split( ",", $line );
+			push @$meta_info, join( ",", ( $sample, @rest ) );
+			my $right_mate = $left_mate;
+			$right_mate =~ s/_R1_/_R2_/;
+			my $cur_dir = getcwd();
+			if( -f $cur_dir . '/concat_per_sample_fastq/' . $right_mate ) {
+				$$sample_info{ 'samples' }{  $sample } = [ $cur_dir . '/concat_per_sample_fastq/' . $left_mate, $cur_dir . '/concat_per_sample_fastq/' . $right_mate ];
+			} else {
+				$$sample_info{ 'samples' }{ $sample } = [ $cur_dir . '/concat_per_sample_fastq/' . $left_mate ];
+			}
+		}
+	}
+	close FH or die "Error in closing the file, $meta_file, $!\n";
+	$yaml -> [scalar @$yaml ] -> {'samples'} = $$sample_info{ 'samples' };
+	return ( $yaml, $meta_info );
+}
