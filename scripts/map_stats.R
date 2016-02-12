7f6858d6812523d091a5d2d86efe00885112d8e3
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-02-12 12:45:51 -0500

    added custom labels (Ex: 20M, 30M ...) to y-axis

--- scripts/map_stats.R
+++ scripts/map_stats.R
@@ -15,9 +15,21 @@ png( args[2], width = 8, height = 8, unit="in",res=300 )
 
 upper_limit <- max(x$Total_Reads)
 limits <- seq( 0, upper_limit, length.out=10)
+
+cust_labels <- vector("character",length=length(limits))
+
+if( nchar(upper_limit) < 7 ) {
+  cust_labels <- paste(round(limits/1000),"K",sep="") 
+  limits <- round(limits/1000) * 1000
+} else {
+  cust_labels <- paste(round(limits/1000000),"M",sep="") 
+  limits <- round(limits/1000000) * 1000000
+}
+
+
 colors <- c(Total_Reads="Grey", Unique_Reads="Blue")
 
-ggplot(x1, aes(x=Sample, y=value, fill=variable)) + geom_bar( stat = "identity", position="identity") + scale_y_continuous("",limits=c(0,upper_limit), labels=comma, breaks=limits) + scale_fill_manual(values=colors) + labs( title="Read Alignment Report\n\n", x = "Sample Names", y="") + guides(fill=guide_legend(title=NULL)) + theme_bw() + theme(axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5, size=10))
+ggplot(x1, aes(x=Sample, y=value, fill=variable)) + geom_bar( stat = "identity", position="identity") + scale_y_continuous("",limits=c(0,upper_limit), labels=cust_labels, breaks=limits) + scale_fill_manual(values=colors) + labs( title="Read Alignment Report\n\n", x = "Sample Names", y="") + guides(fill=guide_legend(title=NULL)) + theme_bw() + theme(axis.text.x = element_text(angle=90, hjust = 1, vjust=0.5, size=10))
 
 dev.off()
 
